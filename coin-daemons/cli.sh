8ffcc0045c67520fce6d8cddfec6a2ee024a91e2
me <also-me>
2024-08-30 22:27:51 -0400

    small cli changes

--- coin-daemons/cli.sh
+++
@@ -1,5 +0,0 @@
-RPC_PORT="19332"
-RPC_USER="kafre-ltc-signet-user"
-RPC_PASS="4G6mXl0Le+ViKRmy/jCeek1CrRxktGmipcokvrnHUGaX2waCwPdeno0sU9gANyqAQFVkT6AVJNd7V3C7H9wxwA=="
-
-./cli/ltc -rpcconnect="rpc.test.ltc.jonahstorch.com" -rpcport="$RPC_PORT" -rpcpassword="$RPC_PASS" -rpcuser="$RPC_USER" getblockcount
