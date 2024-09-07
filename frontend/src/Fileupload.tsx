77ff2cb5f4b54f68fb1978531e6c4cd3f52f7b24
jsparson1 <94084697+jsparson1@users.noreply.github.com>
2024-09-07 18:35:47 -0400

    refactor

--- frontend/src/Fileupload.tsx
+++ frontend/src/Fileupload.tsx
@@ -3,20 +3,30 @@ import './App.css'
 
 const FileUpload: React.FC = () => {
     //todo remove nulls add better logging
-    const [selectedFile, setSelectedFile] = useState<File | null>(null);
-    const [file, setFile] = useState<File | null>(null);
-    const [hashResult, setHashResult] = useState<string>("No File Registered");
+    const [verificationfile, setVerificationFile] = useState<File | null>(null);
+    const [verificationHashResult, setVerificationHashResult] = useState<string>("No File Registered");
     const [error, setError] = useState<string | null>(null);
 
-    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleVerificationFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const selectedFile = e.target.files?.[0];
         if (selectedFile) {
-            setFile(selectedFile);
-            hashFile(selectedFile);
+            setVerificationFile(selectedFile);
+            hashVerificationFile(selectedFile);
         }
     };
 
-    const hashFile = useCallback(async (file: File,) => {
+    const [registrationFile, setRegistrationFile] = useState<File | null>(null);
+    const [registrationHash, setRegistrationHash] = useState<string>("No File Registered");
+
+    const handleRegistrationFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedRegistrationFile = e.target.files?.[0];
+        if (selectedRegistrationFile) {
+            setRegistrationFile(selectedRegistrationFile);
+            hashRegistrationFile(selectedRegistrationFile);
+        }
+    };
+
+    const hashRegistrationFile = useCallback(async (file: File,) => {
         const reader = new FileReader();
         reader.onload = async (e) => {
             try {
@@ -27,30 +37,66 @@ const FileUpload: React.FC = () => {
                 const hashArray = Array.from(new Uint8Array(hashBuffer));
 
                 const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-                setHashResult(hashHex);
+                setRegistrationHash(hashHex);
 
-                console.log(hashResult)
+                //console.log(hashResult)
 
                 setError(null);
             } catch (err) {
                 console.log('Error hashing file: ' + (err instanceof Error ? err.message : String(err)));
 
-                setHashResult("ERROR");
+                setRegistrationHash("ERROR");
             }
         };
         reader.onerror = () => {
             setError('Error reading file');
-            setHashResult("");
+            setRegistrationHash("");
         };
         reader.readAsArrayBuffer(file);
     }, []);
 
-    const handleUpload = () => {
+
+    const hashVerificationFile = useCallback(async (file: File,) => {
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            try {
+                const arrayBuffer = e.target?.result as ArrayBuffer;
+                console.log(arrayBuffer)
+                const hashBuffer = await crypto.subtle.digest('SHA-256', arrayBuffer);
+                console.log(hashBuffer)
+                const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+                const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+                setVerificationHashResult(hashHex);
+
+                //console.log(hashResult)
+
+                setError(null);
+            } catch (err) {
+                console.log('Error hashing file: ' + (err instanceof Error ? err.message : String(err)));
+
+                setVerificationHashResult("ERROR");
+            }
+        };
+        reader.onerror = () => {
+            setError('Error reading file');
+            setVerificationHashResult("");
+        };
+        reader.readAsArrayBuffer(file);
+    }, []);
+
+
+
+
+
+
+
+    const handleRegistration = () => {
 
         // replace with test url
         fetch('http://127.0.0.1:3000/register', {
             method: 'POST',
-            body: hashResult,
+            body: registrationHash,
         })
             .then((response) => response.json())
             .then((data) => {
@@ -62,14 +108,31 @@ const FileUpload: React.FC = () => {
 
     };
 
+    const handleVerify = () => {
+
+        fetch('http://127.0.0.1:3000/verify', {
+            method: 'POST',
+            body: verificationHashResult,
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+
+                console.error('Error:', error);
+            });
+
+    };
+
     return (
         <div>
             <div className='upload-card'>
                 <div className='card-content'>
                     <div >Get A File Timestamp</div>
                     <div>
-                        <input type="file" onChange={handleFileChange} />
-                        <button onClick={handleUpload} disabled={!selectedFile}>
+                        <input type="file" onChange={handleVerificationFileChange} />
+                        <button onClick={handleVerify} disabled={false}>
                             Check
                         </button>
                     </div>
@@ -80,8 +143,8 @@ const FileUpload: React.FC = () => {
                 <div className='card-content'>
                     <div >Timestamp A File</div>
                     <div>
-                        <input type="file" onChange={handleFileChange} />
-                        <button onClick={handleUpload} disabled={false}>
+                        <input type="file" onChange={handleRegistrationFileChange} />
+                        <button onClick={handleRegistration} disabled={false}>
                             Register
                         </button>
                     </div>
